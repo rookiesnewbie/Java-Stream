4152b02d3de4865565ecf0a94ef08fc80dc10c56
rookiesnewbie <2049448867@qq.com>
2023-10-09 11:07:22 +0800

    创建不可变的list集合

---
+++ 创建不可变集合/ImmutableOfList.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+/**
+ * @Author LiTeng
+ * @Date 2023/10/9 10:57
+ * Version 1.0
+ * @Description 创建不可变的List集合
+ */
+public class ImmutableOfList {
+
+    public static void main(String[] args) {
+        /**
+         * 创建不可变的List集合
+         *  "李白","铠","兰陵王","小乔","大乔","孙策","周瑜","诸葛亮"
+         */
+
+        //一旦创建完成，就不能修改
+        List<String> list = List.of("李白","铠","兰陵王","小乔","大乔","孙策","周瑜","诸葛亮");
+
+        list.add("zs");
+
+    }
+
+}
